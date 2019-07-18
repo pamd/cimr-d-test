c0c526b2045a46687965a49ca4ef1b9c8a433868
dongbohu <dongbo.hu@gmail.com>
2019-07-18 12:46:09 -0400

    New S3 path and new request

--- .circleci/handle_request.sh
+++ .circleci/handle_request.sh
@@ -15,22 +15,19 @@ if [ -z $CIRCLE_PULL_REQUEST ]; then
     exit 0
 fi
 
-# dhu: test only (remove this block when tests are done)
-#if [ -z $CIRCLE_PULL_REQUEST ]; then
-#    PR_NUMBER=test
-#fi
-
 # Extract PR number from CIRCLE_PULL_REQUEST, see more options at:
 # https://stackoverflow.com/q/3162385
 PR_NUMBER=$(echo ${CIRCLE_PULL_REQUEST##*/})
 
-REQ_INDICATOR="req_success.txt"
+# Key of the S3 object whose existence indicates that user request has been
+# handled successfully.
+INDICATOR_KEY="test-only/work-in-progress/PR-${PR_NUMBER}/req_success.txt"
 
 # Install awscli for "aws" command
 sudo pip install awscli
 
 # Delete the indicator file (if it exists)
-aws s3 rm s3://cimr-root/submitted_data/PR_${PR_NUMBER}/$REQ_INDICATOR
+aws s3api delete-object --bucket cimr-root --key ${INDICATOR_KEY}
 
 # Parse user request, download data, and extract the tarball file
 python3 .circleci/parse_yaml.py
@@ -38,19 +35,15 @@ python3 .circleci/parse_yaml.py
 # Process submitted data
 python3 .circleci/process_submitted_data.py
 
-# Save submitted data to private S3 bucket
+# Save submitted data to a temporary location in private S3 bucket
 if [ -d submitted_data ]; then
-    aws s3 sync submitted_data s3://cimr-root/test-submitted/PR_${PR_NUMBER}/
+    aws s3 sync submitted_data s3://cimr-root/test-only/work-in-progress/PR-${PR_NUMBER}/
 fi
 
-# Save processed data to private S3 bucket as well
+# Save processed data to a temporary location in public S3 bucket
 if [ -d processed_data ]; then
-    aws s3 sync processed_data s3://cimr-root/test-processed/PR_${PR_NUMBER}/
+    aws s3 sync processed_data s3://cimr-d/test-only/work-in-progress/PR-${PR_NUMBER}/
 fi
 
-# Create a new local indicator file to tell whether user request is handled successfully
-rm -f /tmp/$REQ_INDICATOR
-touch /tmp/$REQ_INDICATOR
-
-# Copy the indicator file to S3
-aws s3 cp /tmp/$REQ_INDICATOR s3://cimr-root/test-submitted/PR_${PR_NUMBER}/
+# Create a new object in S3 to indicate that user request has been handled successfully
+aws s3api put-object --bucket cimr-root --key ${INDICATOR_KEY}
